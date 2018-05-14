b19fc36a06551d53845a74da0aac9856eb2b976f
wangxuesong <673046307@qq.com>
2018-05-14 10:35:50 +0800

    run on PC

--- visualize.py
+++ visualize.py
@@ -115,9 +115,9 @@ def animation_with_label(out_prob,batchdata,decision_bound,parameters=None):
     anim2 = FuncAnimation(fig, update2,frames= range(len(batchdata)),interval=100)
 
     # Set up formatting for the movie files
-    anim.save('data/hyperplane/z_plot.mp4', writer="ffmpeg")
-    anim2.save('data/hyperplane/x_plot.mp4',writer="ffmpeg")
-    # plt.show()
+    # anim.save('data/hyperplane/z_plot.mp4', writer="ffmpeg")
+    # anim2.save('data/hyperplane/x_plot.mp4',writer="ffmpeg")
+    plt.show()
 
 
 def animation(batchdata,parameters=None):
